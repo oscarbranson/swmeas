9c3441e2f9f1dc3184a3c0dac5a58673545cdf77
Oscar Branson <oscarbranson@gmail.com>
2017-06-27 12:05:50 +1000

    import logger

--- swmeas/__init__.py
+++ swmeas/__init__.py
@@ -1,7 +1,8 @@
 import sys
-from CO2_sensor import *
-from O2_sensor import *
-from live_plot import liveplot
+from .CO2_sensor import *
+from .O2_sensor import *
+from .live_plot import liveplot
+from .logger import *
 
 if sys.version_info == (3,):
     sys.exit('Does not work in Python 3.')
