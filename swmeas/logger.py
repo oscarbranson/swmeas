068cde86e2858c8303f32bf1850f7a2a8d592ffc
Oscar Branson <oscarbranson@gmail.com>
2017-06-30 13:31:14 +1000

    sensor auto_identification, improved timing mechanics.

--- swmeas/logger.py
+++ swmeas/logger.py
@@ -4,12 +4,13 @@ import warnings
 
 from .O2_sensor import O2_sensor
 from .CO2_sensor import CO2_sensor
-from .helpers import read_par, write_par, most_recent_json, timed_dir
+from .helpers import read_par, write_par, most_recent_json, timed_dir, find_sensor
 
 
 def logAll(data_dir='./log_data/', interval=60, stop=0,
            O2_n=5, O2_wait=0.5, CO2_n=5, CO2_wait=1.,
-           O2_ID='FT1HQ4GE', CO2_ID='FTHBSQZ9', new_folder_every=None, **kwargs):
+           O2_ID=None, CO2_ID=None, sensor_json=None,
+           new_folder_every=None, O2_mode='water', verbose=False, **kwargs):
     """
     Log CO2, O2 and Temp sequentially, and save to files in data_dir.
 
@@ -42,53 +43,74 @@ def logAll(data_dir='./log_data/', interval=60, stop=0,
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
 
-    # create timed subdirectory, if required
-    if new_folder_every is not None:
-        data_dir = timed_dir(data_dir, new_folder_every)
+    # if ID not specified, find a sensor listed in json file
+    if CO2_ID is None:
+        CO2_ID, CO2_name, CO2_port = find_sensor('CO2', sensor_json)
+    else:
+        CO2_name = ''
+        CO2_port = None
+    # if ID not specified, find a sensor listed in json file
+    if O2_ID is None:
+        O2_ID, O2_name, O2_port = find_sensor('TempO2', sensor_json)
+    else:
+        O2_name = ''
+        O2_port = None
 
     # initialize sensors
-    o2 = O2_sensor(O2_ID)
-    co2 = CO2_sensor(CO2_ID)
+    o2 = O2_sensor(ID=O2_ID, port=O2_port, name=O2_name)
+    co2 = CO2_sensor(ID=CO2_ID, port=CO2_port, name=CO2_name)
 
     print('Logging...')
 
     # set up timing
     start_time = os.times()[-1]
     time_now = start_time
-    end_time = time_now + stop
+    run = True
 
-    while (time_now < end_time) | (time_now == end_time):
+    while run:
+        # create timed subdirectory, if required
+        if new_folder_every is not None:
+            save_dir = timed_dir(data_dir, new_folder_every)
+        else:
+            save_dir = data_dir
+        # do the logging
         time_startloop = os.times()[-1]
-        print('Elapsed Time: {}'.format(time_startloop - start_time))
-        print('  CO2 Measurement')
+        elapsed = time_startloop - start_time  # total elapsed time at start of loop
+        # print('Elapsed Time: {}'.format(time_startloop - start_time))
+        # print('  CO2 Measurement')
         co2.read_multi(CO2_n, CO2_wait)
-        co2.write_batch(data_dir + '/co2.csv')
-        print(co2.write_str)
-        print('')
+        co2.write_batch(save_dir + '/co2.csv')
+        if verbose:
+            print(co2.write_str[:-2])
+        # print('')
 
-        print('  O2-Temp Measurement')
+        # print('  O2-Temp Measurement')
         o2.read_multi(O2_n, O2_wait)
-        o2.write_TempO2_batch(data_dir + '/temp.csv', data_dir + '/o2.csv')
-        o2.write(data_dir + '/TempO2_raw.csv')
-        print(o2.write_str)
-        print('')
+        o2.write_TempO2_batch(save_dir + '/temp.csv', save_dir + '/o2.csv', mode=O2_mode)
+        o2.write(save_dir + '/TempO2_raw.csv')
+        if verbose:
+            print(o2.write_str[:-2])
+        # print('')
 
-        if stop > 0:
-            # if stop given, increment time_now so the loop eventually ends
-            time_now = round(os.times()[-1])
-
+        # timing mechanics
         loop_time = os.times()[-1] - time_startloop
-        print('loop time: {}'.format(loop_time))
+        # print('loop time: {:.1f}'.format(loop_time))
+        if stop > 0:
+            # if the next interval's start time > stop time
+            if elapsed + loop_time + interval > stop:
+                print('\nFinished.')
+                break  # stop the loop
         if loop_time < interval:
             sleeptime = interval - loop_time
-            print('sleep time: {}'.format(sleeptime))
+            # print('sleep time: {:.1f}'.format(sleeptime))
             time.sleep(sleeptime)
 
     return
 
 
-def logCO2(data_dir='./log_data/', interval=60, stop=0,
-           CO2_n=5, CO2_wait=1., CO2_ID='FTHBSQZ9', new_folder_every=None, **kwargs):
+def logCO2(data_dir='./log_data/', interval=30, stop=0,
+           CO2_n=5, CO2_wait=1., CO2_ID=None, sensor_json=None,
+           new_folder_every=None, verbose=False, **kwargs):
     """
     Log CO2 and save to files in data_dir.
 
@@ -112,52 +134,63 @@ def logCO2(data_dir='./log_data/', interval=60, stop=0,
         The serial number of the CO2 sensor
     """
     # record parameters
-    write_par(locals(), data_dir + '/logAll.json')
-
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
+    write_par(locals(), data_dir + '/logAll.json')
 
-    # create timed subdirectory, if required
-    if new_folder_every is not None:
-        data_dir = timed_dir(data_dir, new_folder_every)
+    # if ID not specified, find a sensor listed in json file
+    if CO2_ID is None:
+        CO2_ID, CO2_name, CO2_port = find_sensor('CO2', sensor_json)
+    else:
+        CO2_name = ''
+        CO2_port = None
 
     # initialize sensors
-    co2 = CO2_sensor(CO2_ID)
+    co2 = CO2_sensor(ID=CO2_ID, port=CO2_port, name=CO2_name)
 
-    print('Logging...')
+    print('Logging CO2...')
 
     # set up timing
     start_time = os.times()[-1]
     time_now = start_time
-    end_time = time_now + stop
+    run = True
 
-    while (time_now < end_time) | (time_now == end_time):
-        time_startloop = os.times()[-1]
-        print('Elapsed Time: {}'.format(time_startloop - start_time))
-        print('  CO2 Measurement')
+    while run:
+        # create timed subdirectory, if required
+        if new_folder_every is not None:
+            save_dir = timed_dir(data_dir, new_folder_every)
+        else:
+            save_dir = data_dir
+        # do the logging
+        time_startloop = os.times()[-1]  # time at start of loop
+        elapsed = time_startloop - start_time  # total elapsed time at start of loop
+        # print('Elapsed Time: {:.1f}'.format(elapsed))
+        # print('  CO2 Measurement')
         co2.read_multi(CO2_n, CO2_wait)
-        co2.write_batch(data_dir + '/co2.csv')
-        print(co2.write_str)
-        print('')
-        
-        if stop > 0:
-            # if stop given, increment time_now so the loop eventually ends
-            time_now = round(os.times()[-1])
+        co2.write_batch(save_dir + '/co2.csv')
+        if verbose:
+            print(co2.write_str[:-2])
+        # print('')
 
         # timing mechanics
         loop_time = os.times()[-1] - time_startloop
-        print('loop time: {}'.format(loop_time))
+        # print('loop time: {:.1f}'.format(loop_time))
+        if stop > 0:
+            # if the next interval's start time > stop time
+            if elapsed + loop_time + interval > stop:
+                print('\nFinished.')
+                break  # stop the loop
         if loop_time < interval:
             sleeptime = interval - loop_time
-            print('sleep time: {}'.format(sleeptime))
+            # print('sleep time: {:.1f}'.format(sleeptime))
             time.sleep(sleeptime)
 
     return
 
 
-def logTempO2(data_dir='./log_data/', interval=60, stop=0,
-              O2_n=5, O2_wait=.5, O2_ID='FT1HQ4GE',
-              mode='water', new_folder_every=None, **kwargs):
+def logTempO2(data_dir='./log_data/', interval=30, stop=0,
+              O2_n=5, O2_wait=.5, O2_ID=None, sensor_json=None,
+              mode='water', new_folder_every=None, verbose=False, **kwargs):
     """
     Log O2 and Temp and save to files in data_dir.
 
@@ -181,45 +214,57 @@ def logTempO2(data_dir='./log_data/', interval=60, stop=0,
         The serial number of the CO2 sensor
     """
     # record parameters
-    write_par(locals(), data_dir + '/logAll.json')
-
     if not os.path.exists(data_dir):
         os.mkdir(data_dir)
 
-    # create timed subdirectory, if required
-    if new_folder_every is not None:
-        data_dir = timed_dir(data_dir, new_folder_every)
+    write_par(locals(), data_dir + '/logAll.json')
+
+    # if ID not specified, find a sensor listed in json file
+    if O2_ID is None:
+        O2_ID, O2_name, O2_port = find_sensor('TempO2', sensor_json)
+    else:
+        O2_name = ''
+        O2_port = None
 
     # initialize sensors
-    o2 = O2_sensor(O2_ID)
+    o2 = O2_sensor(ID=O2_ID, port=O2_port, name=O2_name)
 
     print('Logging...')
 
     # set up timing
     start_time = os.times()[-1]
     time_now = start_time
-    end_time = time_now + stop
+    run = True
 
-    while (time_now < end_time) | (time_now == end_time):
-        time_startloop = os.times()[-1]
-        print('Elapsed Time: {}'.format(time_startloop - start_time))
-
-        print('  O2-Temp Measurement')
+    while run:
+        # create timed subdirectory, if required
+        if new_folder_every is not None:
+            save_dir = timed_dir(data_dir, new_folder_every)
+        else:
+            save_dir = data_dir
+        # do the logging
+        time_startloop = os.times()[-1]  # time at start of loop
+        elapsed = time_startloop - start_time  # total elapsed time at start of loop
+        # print('Elapsed Time: {:.1f}'.format(time_startloop - start_time))
+        # print('  O2-Temp Measurement')
         o2.read_multi(O2_n, O2_wait)
-        o2.write_TempO2_batch(data_dir + '/temp.csv', data_dir + '/o2.csv')
-        print(o2.write_str)
-        o2.write(data_dir + '/TempO2_raw.csv')
-        print('')
-
-        if stop > 0:
-            # if stop given, increment time_now so the loop eventually ends
-            time_now = round(os.times()[-1])
-
+        o2.write_TempO2_batch(save_dir + '/temp.csv', save_dir + '/o2.csv', mode=mode)
+        if verbose:
+            print(o2.write_str[:-2])
+        o2.write(save_dir + '/TempO2_raw.csv')
+        # print('')
+        
+        # timing mechanics
         loop_time = os.times()[-1] - time_startloop
-        print('loop time: {}'.format(loop_time))
+        # print('loop time: {:.1f}'.format(loop_time))
+        if stop > 0:
+            # if the next interval's start time > stop time
+            if elapsed + loop_time + interval > stop:
+                print('\nFinished.')
+                break  # stop the loop
         if loop_time < interval:
             sleeptime = interval - loop_time
-            print('sleep time: {}'.format(sleeptime))
+            # print('sleep time: {:.1f}'.format(sleeptime))
             time.sleep(sleeptime)
 
     return
