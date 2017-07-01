c0afaaec37e66353c6e5b6675f38b8a8743292fd
Oscar Branson <oscarbranson@gmail.com>
2017-07-02 00:03:47 +1000

    clean up logger functions.
    * logAll redundant
    * generalised logXXX functions

--- swmeas/logger.py
+++ swmeas/logger.py
@@ -7,109 +7,8 @@ from .CO2_sensor import CO2_sensor
 from .helpers import read_par, write_par, most_recent_json, timed_dir, find_sensor
 
 
-def logAll(data_dir='./log_data/', interval=60, stop=0,
-           O2_n=5, O2_wait=0.5, CO2_n=5, CO2_wait=1.,
-           O2_ID=None, CO2_ID=None, sensor_json=None,
-           new_folder_every=None, O2_mode='water', verbose=False, **kwargs):
-    """
-    Log CO2, O2 and Temp sequentially, and save to files in data_dir.
-
-    Parameters
-    ----------
-    data_dir : str
-        folder in which to store the data files.
-    interval : float
-        Time between measurements (seconds). Note that if this less than
-        the measurement time, measurements will be run continuously
-    stop : float
-        How long you want the loop to run for (seconds). If zero,
-        the loop will run until interrupted.
-    O2_n : int
-        The number of O2 measurements to make per loop.
-    O2_n : float
-        Time between individual O2 measurements.
-    CO2_n : int
-        The number of CO2 measurements to make per loop.
-    CO2_wait : float
-        Time between individual CO2 measurements.
-    O2_ID : str
-        The serial number of the O2 sensor
-    CO2_ID : str
-        The serial number of the CO2 sensor
-    """
-    # record parameters
-    write_par(locals(), data_dir + '/logAll.json')
-
-    if not os.path.exists(data_dir):
-        os.mkdir(data_dir)
-
-    # if ID not specified, find a sensor listed in json file
-    if CO2_ID is None:
-        CO2_ID, CO2_name, CO2_port = find_sensor('CO2', sensor_json)
-    else:
-        CO2_name = ''
-        CO2_port = None
-    # if ID not specified, find a sensor listed in json file
-    if O2_ID is None:
-        O2_ID, O2_name, O2_port = find_sensor('TempO2', sensor_json)
-    else:
-        O2_name = ''
-        O2_port = None
-
-    # initialize sensors
-    o2 = O2_sensor(ID=O2_ID, port=O2_port, name=O2_name)
-    co2 = CO2_sensor(ID=CO2_ID, port=CO2_port, name=CO2_name)
-
-    print('Logging...')
-
-    # set up timing
-    start_time = os.times()[-1]
-    time_now = start_time
-    run = True
-
-    while run:
-        # create timed subdirectory, if required
-        if new_folder_every is not None:
-            save_dir = timed_dir(data_dir, new_folder_every)
-        else:
-            save_dir = data_dir
-        # do the logging
-        time_startloop = os.times()[-1]
-        elapsed = time_startloop - start_time  # total elapsed time at start of loop
-        # print('Elapsed Time: {}'.format(time_startloop - start_time))
-        # print('  CO2 Measurement')
-        co2.read_multi(CO2_n, CO2_wait)
-        co2.write_batch(save_dir + '/co2.csv')
-        if verbose:
-            print(co2.write_str[:-1])
-        # print('')
-
-        # print('  O2-Temp Measurement')
-        o2.read_multi(O2_n, O2_wait)
-        o2.write_TempO2_batch(save_dir + '/temp.csv', save_dir + '/o2.csv', mode=O2_mode)
-        o2.write(save_dir + '/TempO2_raw.csv')
-        if verbose:
-            print(o2.write_str[:-1])
-        # print('')
-
-        # timing mechanics
-        loop_time = os.times()[-1] - time_startloop
-        # print('loop time: {:.1f}'.format(loop_time))
-        if stop > 0:
-            # if the next interval's start time > stop time
-            if elapsed + loop_time + interval > stop:
-                print('\nFinished.')
-                break  # stop the loop
-        if loop_time < interval:
-            sleeptime = interval - loop_time
-            # print('sleep time: {:.1f}'.format(sleeptime))
-            time.sleep(sleeptime)
-
-    return
-
-
 def logCO2(data_dir='./log_data/', interval=30, stop=0,
-           CO2_n=5, CO2_wait=1., CO2_ID=None, sensor_json=None,
+           n=5, wait=1., ID=None, sensor_json=None,
            new_folder_every=None, verbose=False, **kwargs):
     """
     Log CO2 and save to files in data_dir.
@@ -126,12 +25,13 @@ def logCO2(data_dir='./log_data/', interval=30, stop=0,
     stop : float
         How long you want the loop to run for (seconds). If zero,
         the loop will run until interrupted.
-    CO2_n : int
+    n : int
         The number of CO2 measurements to make per loop.
-    CO2_wait : float
+    wait : float
         Time between individual CO2 measurements.
-    CO2_ID : str
-        The serial number of the CO2 sensor
+    ID : str
+        A unique identifier of the sensor (e.g. serial number). If None
+        a sensor of the correct type is found automatically.
     """
     # record parameters
     if not os.path.exists(data_dir):
@@ -139,18 +39,14 @@ def logCO2(data_dir='./log_data/', interval=30, stop=0,
     write_par(locals(), data_dir + '/logCO2.json')
 
     # if ID not specified, find a sensor listed in json file
-    if CO2_ID is None:
-        CO2_ID, CO2_name, CO2_port = find_sensor('CO2', sensor_json)
+    if ID is None:
+        co2 = CO2_sensor()  # find sensor automatically
     else:
-        CO2_name = ''
-        CO2_port = None
-
-    # initialize sensors
-    co2 = CO2_sensor(ID=CO2_ID, port=CO2_port, name=CO2_name)
+        co2 = CO2_sensor(ID=ID)
 
     print('Logging CO2...')
 
-    # set up timing
+    # initialize sensor
     start_time = os.times()[-1]
     time_now = start_time
     run = True
@@ -166,7 +62,7 @@ def logCO2(data_dir='./log_data/', interval=30, stop=0,
         elapsed = time_startloop - start_time  # total elapsed time at start of loop
         # print('Elapsed Time: {:.1f}'.format(elapsed))
         # print('  CO2 Measurement')
-        co2.read_multi(CO2_n, CO2_wait)
+        co2.read_multi(n, wait)
         co2.write_batch(save_dir + '/co2.csv')
         if verbose:
             print(co2.write_str[:-1])
@@ -189,7 +85,7 @@ def logCO2(data_dir='./log_data/', interval=30, stop=0,
 
 
 def logTempO2(data_dir='./log_data/', interval=30, stop=0,
-              O2_n=5, O2_wait=.5, O2_ID=None, sensor_json=None,
+              n=5, wait=.5, ID=None, sensor_json=None,
               mode='water', new_folder_every=None, verbose=False, **kwargs):
     """
     Log O2 and Temp and save to files in data_dir.
@@ -206,12 +102,13 @@ def logTempO2(data_dir='./log_data/', interval=30, stop=0,
     stop : float
         How long you want the loop to run for (seconds). If zero,
         the loop will run until interrupted.
-    O2_n : int
+    n : int
         The number of O2 measurements to make per loop.
-    O2_n : float
+    wait : float
         Time between individual O2 measurements.
-    O2_ID : str
-        The serial number of the CO2 sensor
+    ID : str
+        A unique identifier of the sensor (e.g. serial number). If None
+        a sensor of the correct type is found automatically.
     """
     # record parameters
     if not os.path.exists(data_dir):
@@ -219,15 +116,11 @@ def logTempO2(data_dir='./log_data/', interval=30, stop=0,
 
     write_par(locals(), data_dir + '/logTempO2.json')
 
-    # if ID not specified, find a sensor listed in json file
-    if O2_ID is None:
-        O2_ID, O2_name, O2_port = find_sensor('TempO2', sensor_json)
+    # initialize sensor
+    if ID is None:
+        o2 = O2_sensor()  # find sensor automatically
     else:
-        O2_name = ''
-        O2_port = None
-
-    # initialize sensors
-    o2 = O2_sensor(ID=O2_ID, port=O2_port, name=O2_name)
+        o2 = O2_sensor(ID=ID)
 
     print('Logging TempO2...')
 
@@ -247,7 +140,7 @@ def logTempO2(data_dir='./log_data/', interval=30, stop=0,
         elapsed = time_startloop - start_time  # total elapsed time at start of loop
         # print('Elapsed Time: {:.1f}'.format(time_startloop - start_time))
         # print('  O2-Temp Measurement')
-        o2.read_multi(O2_n, O2_wait)
+        o2.read_multi(n, wait)
         o2.write_TempO2_batch(save_dir + '/temp.csv', save_dir + '/o2.csv', mode=mode)
         if verbose:
             print(o2.write_str[:-1])
@@ -290,8 +183,7 @@ def auto_log(mode='All', path=None, param_file=None):
 
     """
     fndict = {'CO2': logCO2,
-              'TempO2': logTempO2,
-              'All': logAll}
+              'TempO2': logTempO2}
 
     if mode in fndict:
         fn = fndict[mode]
