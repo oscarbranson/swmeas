79fbf818c31a4b0a9d1726185fb21e2df78def78
Oscar <oscarbranson@gmail.com>
2018-07-02 12:58:36 +1000

    update for Gascard CO2 sensor

--- swmeas/sensors.py
+++ swmeas/sensors.py
@@ -70,134 +70,133 @@ class dummy_sensor(object):
             f.write(out_str + '\n')
 
 
-class CO2_sensor(object):
-    """
-    Connect to and take measurements from a K-30 CO2 Sensor.
+# class CO2_sensor(object):
+#     """
+#     Connect to and take measurements from a K-30 CO2 Sensor.
 
-    Parameters
-    ----------
-    ID : str
-        The serial number of the CO2 Sensor
-    msg : byte str
-        The message sent to the sensor to retrieve
-        a measurement (should not need to be changed).
-    """
+#     Parameters
+#     ----------
+#     ID : str
+#         The serial number of the CO2 Sensor
+#     msg : byte str
+#         The message sent to the sensor to retrieve
+#         a measurement (should not need to be changed).
+#     """
 
-    def __init__(self, ID=None, port=None, name=''):
-        self.ID = ID
-        self.name = name
-        self.port = port
-        self.last_read = None
-        self.connect()
+#     def __init__(self, ID=None, port=None, name=''):
+#         self.ID = ID
+#         self.name = name
+#         self.port = port
+#         self.last_read = None
+#         self.connect()
 
-    # sensor-specific setup
-    def connect(self):
-        """
-        Connects to CO2 Sensor identified by Serial Number.
-        """
-        if self.port is not None:
-            p = portscan(self.port)
-            if p is not None:
-                self.port = p.device
-                self.ID = p.serial_number
-                self.name = get_sensor_name(self.ID)
-            else:
-                raise serial.SerialException("Can't find port: {}".format(self.port))
-        elif self.ID is not None:
-            p = portscan(self.ID)
-            if p is not None:
-                self.port = p.device
-                self.ID = p.serial_number
-                self.name = get_sensor_name(self.ID)
-            else:
-                raise serial.SerialException("Can't find port with ID: {}".format(self.ID))
-        else:
-            self.ID, self.name, self.port = find_sensor('CO2')
+#     # sensor-specific setup
+#     def connect(self):
+#         """
+#         Connects to CO2 Sensor identified by Serial Number.
+#         """
+#         if self.port is not None:
+#             p = portscan(self.port)
+#             if p is not None:
+#                 self.port = p.device
+#                 self.ID = p.serial_number
+#                 self.name = get_sensor_name(self.ID)
+#             else:
+#                 raise serial.SerialException("Can't find port: {}".format(self.port))
+#         elif self.ID is not None:
+#             p = portscan(self.ID)
+#             if p is not None:
+#                 self.port = p.device
+#                 self.ID = p.serial_number
+#                 self.name = get_sensor_name(self.ID)
+#             else:
+#                 raise serial.SerialException("Can't find port with ID: {}".format(self.ID))
+#         else:
+#             self.ID, self.name, self.port = find_sensor('CO2')
 
-        self.label = "CO2 sensor {} ({}) on port {}\n".format(self.name, self.ID, self.port)
-        print("\n" + '*' * len(self.label) + '\n' +
-              self.label +
-              '*' * len(self.label) + '\n')
+#         self.label = "CO2 sensor {} ({}) on port {}\n".format(self.name, self.ID, self.port)
+#         print("\n" + '*' * len(self.label) + '\n' +
+#               self.label +
+#               '*' * len(self.label) + '\n')
 
-        self.sensor = serial.Serial(self.port, baudrate=9600, timeout=.5)
-        return
+#         self.sensor = serial.Serial(self.port, baudrate=9600, timeout=.5)
+#         return
 
-    def disconnect(self):
-        """
-        Close connection to sensor (shouldn't be necessary).
-        """
-        self.sensor.close()
-        return
+#     def disconnect(self):
+#         """
+#         Close connection to sensor (shouldn't be necessary).
+#         """
+#         self.sensor.close()
+#         return
 
-    # data recording functions
-    def read(self):
-        """
-        Read a single CO2 measurement from the sensor.
+#     # data recording functions
+#     def read(self):
+#         """
+#         Read a single CO2 measurement from the sensor.
 
-        Returns
-        -------
-        float : ppm CO2
-        """
-        # time at start of measurement
-        tnow = time_now()
+#         Returns
+#         -------
+#         float : ppm CO2
+#         """
+#         # time at start of measurement
+#         tnow = time_now()
 
-        self.sensor.flushInput()
-        msg = b"\xFE\x44\x00\x08\x02\x9F\x25"
-        self.sensor.write(msg)
-        # read measurement from sensor
-        resp = bytes(self.sensor.read(7))
-        high = resp[3]
-        low = resp[4]
-        co2 = (high * 256.) + low
+#         self.sensor.flushInput()
+#         msg = b"\xFE\x44\x00\x08\x02\x9F\x25"
+#         self.sensor.write(msg)
+#         # read measurement from sensor
+#         resp = bytes(self.sensor.read(7))
+#         high = resp[3]
+#         low = resp[4]
+#         co2 = (high * 256.) + low
 
-        self.last_read = [tnow, co2]
-        return self.last_read
+#         self.last_read = [tnow, co2]
+#         return self.last_read
 
-    def read_multi(self, n, wait=2.):
-        """
-        Read multiple CO2 measurements from sensor.
+#     def read_multi(self, n, wait=2.):
+#         """
+#         Read multiple CO2 measurements from sensor.
 
-        Parameters
-        ----------
-        n : int
-            Number of measurements
-        wait : float
-            Seconds between measurements
+#         Parameters
+#         ----------
+#         n : int
+#             Number of measurements
+#         wait : float
+#             Seconds between measurements
 
-        Returns
-        -------
-        List of ppm CO2 measurements.
-        """
-        out = []
-        for i in range(n):
-            # time at start of measurement
-            out.append(self.read())
-            time.sleep(wait)
+#         Returns
+#         -------
+#         List of ppm CO2 measurements.
+#         """
+#         out = []
+#         for i in range(n):
+#             # time at start of measurement
+#             out.append(self.read())
+#             time.sleep(wait)
 
-        self.last_read = out
-        return self.last_read
+#         self.last_read = out
+#         return self.last_read
 
-    def write_header(self, file):
-        """
-        Writes a file header for the measurements
-        """
-        outstr = '# CO2 Meter {:} (ID:{:})\n'.format(self.name, self.ID)
-        outstr += 'time,CO2\n'
+#     def write_header(self, file):
+#         """
+#         Writes a file header for the measurements
+#         """
+#         outstr = '# CO2 Meter {:} (ID:{:})\n'.format(self.name, self.ID)
+#         outstr += 'time,CO2\n'
 
-        with open(file, 'a+') as f:
-            f.write(outstr)
+#         with open(file, 'a+') as f:
+#             f.write(outstr)
 
-    def write(self, file):
-        """
-        Write data to file.
-        """
-        if not os.path.exists(file):
-            self.write_header(file)
+#     def write(self, file):
+#         """
+#         Write data to file.
+#         """
+#         if not os.path.exists(file):
+#             self.write_header(file)
 
-        out_str = fmt_lines(self.last_read, '{:}')
-        with open(file, 'a+') as f:
-            f.write(out_str + '\n')
-
+#         out_str = fmt_lines(self.last_read, '{:}')
+#         with open(file, 'a+') as f:
+#             f.write(out_str + '\n')
 
 class O2_sensor(object):
     """
@@ -417,9 +416,9 @@ class O2_sensor(object):
             f.write(out_str + '\n')
 
 
-class pH_sensor(object):
+class pH_CO2_sensor(object):
     """
-    Connect to and take measurements from a Durafit pH probe via a LabJack U6-Pro
+    Connect to and take measurements from a Durafit pH probe and a Gascard CO2 sensor via a LabJack U6-Pro
 
     For LabJack Communication Protocol, refer to LabJackPython [1] and the U6-Pro
     User Guide [2]
@@ -442,7 +441,8 @@ class pH_sensor(object):
         # define read commands
         self.comm = {'LJTemp': u6.AIN24(14),
                      'pH': u6.AIN24(2, ResolutionIndex=12, GainIndex=self.gainindex, Differential=self.differential),
-                     'Temp': u6.AIN24(0, ResolutionIndex=9, GainIndex=self.gainindex, Differential=self.differential)}
+                     'Temp': u6.AIN24(0, ResolutionIndex=9, GainIndex=self.gainindex, Differential=self.differential),
+                     'CO2': u6.AIN24(4, ResolutionIndex=12, GainIndex=self.gainindex, Differential=self.differential)}
 
     def connect(self):
         """
@@ -471,20 +471,22 @@ class pH_sensor(object):
         tnow = time_now()
 
         # record bits from LabJack
-        bLJ_temp, bpH_temp, bpH = self.sensor.getFeedback(self.comm['LJTemp'], self.comm['Temp'], self.comm['pH'])
+        bLJ_temp, bpH_temp, bpH, bCO2 = self.sensor.getFeedback(self.comm['LJTemp'], self.comm['Temp'], 
+                                                                self.comm['pH'], self.comm['CO2'])
 
         # convert to temperature / voltage
         LJ_temp = self.sensor.binaryToCalibratedAnalogTemperature(bLJ_temp)
         pH_temp = self.sensor.binaryToCalibratedAnalogVoltage(self.gainindex, bpH_temp)
         pH = self.sensor.binaryToCalibratedAnalogVoltage(self.gainindex, bpH)
+        CO2 = self.sensor.binaryToCalibratedAnalogVoltage(self.gainindex, bCO2)
 
-        self.last_read = [tnow, pH, pH_temp, LJ_temp]
+        self.last_read = [tnow, pH, pH_temp, CO2, LJ_temp]
 
         return self.last_read
 
     def read_multi(self, n, wait=1.):
         """
-        Read Durafit pH and Temperature voltage, and LabJack Temperature.
+        Read Durafit pH and Temperature voltage, Gascard CO2 voltage, and LabJack Temperature.
 
         Returns
         -------
@@ -502,7 +504,7 @@ class pH_sensor(object):
         """
         Writes a file header for the measurements
         """
-        outstr = '# pH measurements via LabJack U6-Pro (GainIndex:{:.0f})\n'.format(self.gainindex)
+        outstr = '# pH and CO2 measurements via LabJack U6-Pro (GainIndex:{:.0f})\n'.format(self.gainindex)
         outstr += 'time,pH,tempProbe,tempLabjack\n'
 
         with open(file, 'a+') as f:
@@ -518,3 +520,105 @@ class pH_sensor(object):
         out_str = fmt_lines(self.last_read, '{:}')
         with open(file, 'a+') as f:
             f.write(out_str + '\n')
+
+# class pH_sensor(object):
+#     """
+#     Connect to and take measurements from a Durafit pH probe via a LabJack U6-Pro
+
+#     For LabJack Communication Protocol, refer to LabJackPython [1] and the U6-Pro
+#     User Guide [2]
+
+#     [1] https://github.com/labjack/LabJackPython
+#     [2] https://labjack.com/sites/default/files/2014/06/U6_UG_Export_20140604.pdf
+
+#     Parameters
+#     ----------
+#     GainIndex : int
+#         The GainIndex used in recording measurements. See `u6.U6().getFeedback()`
+#         documentation.
+#     """
+#     def __init__(self, GainIndex=0, Differential=True):
+#         self.connect()
+#         self.config = self.sensor.configU6()
+#         self.gainindex = GainIndex
+#         self.differential = Differential
+#         self.last_read = None
+#         # define read commands
+#         self.comm = {'LJTemp': u6.AIN24(14),
+#                      'pH': u6.AIN24(2, ResolutionIndex=12, GainIndex=self.gainindex, Differential=self.differential),
+#                      'Temp': u6.AIN24(0, ResolutionIndex=9, GainIndex=self.gainindex, Differential=self.differential)}
+
+#     def connect(self):
+#         """
+#         Connect to U6 LabJack.
+#         """
+#         try:
+#             self.sensor = u6.U6()
+#         except TypeError:
+#             print("Can't find LabJack. Is it connected?")
+
+#     def disconnect(self):
+#         """
+#         Close the LabJack Connection.
+#         """
+#         self.sensor.close()
+
+#     def read(self):
+#         """
+#         Read Durafit pH and Temperature voltage, and LabJack Temperature.
+
+#         Returns
+#         -------
+#         [time, pH voltage, probe temperature voltage, LabJack temperature Kelvin]
+#         """
+#         # time at start of measurement
+#         tnow = time_now()
+
+#         # record bits from LabJack
+#         bLJ_temp, bpH_temp, bpH = self.sensor.getFeedback(self.comm['LJTemp'], self.comm['Temp'], self.comm['pH'])
+
+#         # convert to temperature / voltage
+#         LJ_temp = self.sensor.binaryToCalibratedAnalogTemperature(bLJ_temp)
+#         pH_temp = self.sensor.binaryToCalibratedAnalogVoltage(self.gainindex, bpH_temp)
+#         pH = self.sensor.binaryToCalibratedAnalogVoltage(self.gainindex, bpH)
+
+#         self.last_read = [tnow, pH, pH_temp, LJ_temp]
+
+#         return self.last_read
+
+#     def read_multi(self, n, wait=1.):
+#         """
+#         Read Durafit pH and Temperature voltage, and LabJack Temperature.
+
+#         Returns
+#         -------
+#         n * [time, pH voltage, probe temperature voltage, LabJack temperature Kelvin]
+#         """
+
+#         out = []
+#         for i in range(n):
+#             out.append(self.read())
+#             time.sleep(wait)
+#         self.last_read = out
+#         return out
+
+#     def write_header(self, file):
+#         """
+#         Writes a file header for the measurements
+#         """
+#         outstr = '# pH measurements via LabJack U6-Pro (GainIndex:{:.0f})\n'.format(self.gainindex)
+#         outstr += 'time,pH,tempProbe,tempLabjack\n'
+
+#         with open(file, 'a+') as f:
+#             f.write(outstr)
+
+#     def write(self, file):
+#         """
+#         Write data to file.
+#         """
+#         if not os.path.exists(file):
+#             self.write_header(file)
+
+#         out_str = fmt_lines(self.last_read, '{:}')
+#         with open(file, 'a+') as f:
+#             f.write(out_str + '\n')
